9f7111ef56cb417dfdea98a9bfa6d715928ff9cd
jessyxcs <c14180106@john.petra.ac.id>
2020-04-15 10:50:41 +0700

    1 file modified - Commit Pola A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,13 @@ export class AppComponent  {
   pola = '';
 
   tmp = 0;
+  count = 0;
   SubmitBtn(){ 
     this.hasil = Math.pow(this.num1, this.num2);
     if(this.hasil % 2 == 0){
       this.bilangan = "Bilangan Genap (Pola B)"
       this.tmp = this.num2;
+
       //FOR LOOP 
       for(var i=this.num2; i<=this.hasil; i++){
         for(var j=1; j <= i; j++){
@@ -26,10 +28,28 @@ export class AppComponent  {
         this.pola = this.pola.concat("\n");
       }
     }
+
     else{
       this.bilangan = "Bilangan Ganjil (Pola A)"
-      
+      this.tmp = 1;
+      this.count = 1;
+      //FOR LOOP 
+      for(var i=this.num2;i>=1;i--){
+        for(var j=0; j<i;j++){
+          this.pola = this.pola.concat("*");
+        }
+
+        if(this.count <= this.num1){
+          for(var k=0; k<this.tmp; k++){
+            this.pola = this.pola.concat("o");
+          }
+          this.tmp++;
+          this.count++;
+        }
+        this.pola = this.pola.concat("\n");
+      }
     }
+
   }
 
   ClearBtn(){
